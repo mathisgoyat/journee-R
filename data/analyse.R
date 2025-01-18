4dbc42573865611188e91d29498def1eb6f35835
Mathis Goyat <mathis.goyat@umontreal.ca>
2025-01-18 15:47:04 -0500

    anciens fichiers

---
+++ data/analyse.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(ratdat)
+
+ggplot(data = complete_old, aes(weight,hindfoot_length))+
+  geom_point()
---
+++ git.sh
@@ -0,0 +1,9 @@
+git config --global user.name "mathis goyat"
+git config -- global user.email "mathis.goyat@umontreal.ca"
+
+git config -- global core.autocrlf true
+
+git config -- global core.editor "nano-w"
+
+git config -- global init.defaultBranch main
+
